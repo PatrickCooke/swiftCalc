7f36575d95861cd5dbe64556ff1254c7b4e8142d
PatrickCooke <cookepa1@gmail.com>
2016-05-09 12:15:33 -0400

    Initial Commit

---
+++ SwiftCalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftCalc
+//
+//  Created by Patrick Cooke on 5/9/16.
+//  Copyright © 2016 Patrick Cooke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
